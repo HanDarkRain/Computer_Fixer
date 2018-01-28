86c6d235e0d194396c8f5481781155848ecbf8fe
HanDarkRain <2227211334@qq.com>
2018-01-28 18:18:25 +0800

    Improving input apparence and adding side navigation bar

--- js/index.js
+++ js/index.js
@@ -15,4 +15,68 @@ $(".nav li a.error").on("click",function() {
 	// $("xxxxxxx").on("click",function(){xxxx});
 })
 
-$()
+
+var diagramLabels = ["System problems", 
+					"Common software problems", 
+					"Hardware problems", 
+					"external device problems"];
+
+var diagramData = [0.10, 0.30, 0.44, 0.16];
+
+var paramsdoughnut = {
+	type: "doughnut",
+	data: {
+       labels: diagramLabels,
+        datasets: [{
+            label: 'Distribution of problems',
+            data: diagramData,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 1
+        }]
+	}
+}
+
+var paramsbar = {
+	type: "horizontalBar",
+	data: {
+       labels: diagramLabels,
+        datasets: [{
+            label: 'Distribution of problems',
+            data: diagramData,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 1
+        }]
+	}
+}
+
+var doughnutDiagram = new Chart("doughnut_diagram",paramsdoughnut);
+
+var barDiagram = new Chart("bar_diagram",paramsbar)
+
+var webHeight = document.body.scrollHeight - window.screen.availHeight * 0.9;
+$(window).on("scroll", function(e) {
+    var percentage = $(window).scrollTop() / webHeight * 100;
+    $(".sidenav-progress").css("top", percentage+"%");
+})
