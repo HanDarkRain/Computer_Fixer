8b89deb023029c6103f0b3871102e60131070211
HanDarkRain <2227211334@qq.com>
2018-02-11 20:33:43 +0800

    Concat html files

--- js/index.js
+++ js/index.js
@@ -71,6 +71,11 @@ var paramsbar = {
 	}
 }
 
+var systemTop = 0,
+    softwareTop = 0,
+    hardwareTop = 0,
+    externalTop = 0;
+
 var doughnutDiagram = new Chart("doughnut_diagram",paramsdoughnut);
 
 var barDiagram = new Chart("bar_diagram",paramsbar);
@@ -79,11 +84,119 @@ var webHeight = document.body.scrollHeight - window.screen.availHeight * 0.9;
 $(window).on("scroll", function(e) {
     var percentage = $(window).scrollTop() / webHeight * 100;
     $(".sidenav-progress").css("top", percentage+"%");
+
+    if (window.scrollY > externalTop) {
+        $("#sidenav-text").html("\uf11c External");
+    } else if (window.scrollY > hardwareTop) {
+        $("#sidenav-text").html("\uf0a0 Hardware");
+    } else if (window.scrollY > softwareTop) {
+        $("#sidenav-text").html("\uf0c7 software");
+    } else if (window.scrollY > systemTop) {
+        $("#sidenav-text").html("\uf011 System");
+    } else {
+        $("#sidenav-text").html("You are here");
+    }
 });
+
+$("#sidenav-text").on("click", function() {
+    if (window.scrollY > externalTop) {
+        navTo("External-0");
+    } else if (window.scrollY > hardwareTop) {
+        navTo("Hardware-0");
+    } else if (window.scrollY > softwareTop) {
+        navTo("Software-0");;
+    } else if (window.scrollY > systemTop) {
+        navTo("System-0");
+    } else {
+        $(".sidenav-text").html("You can search or choose a group");
+    }
+})
 
 function resize() {
     webHeight = document.body.scrollHeight - window.screen.availHeight * 0.9;
+    if ( $("#monitor").offset().top > $("#adjustor").offset().top ) {
+        $("#adjustor").css("height", $("#monitor").offset().top - $("#adjustor").offset().top + "px");
+    }
+
+    try {
+        systemTop = $("#System-0").offset().top - 300;
+        softwareTop = $("#Software-0").offset().top - 300;
+        hardwareTop = $("#Hardware-0").offset().top - 300;
+        externalTop = $("#External-0").offset().top - 300;
+    } catch (e) {}
 }
+
 $(window).on("resize", function() {
     resize();
+    NavTo("scene" + currentScene);
 });
+
+function navTo(id) {
+    $("body").animate({scrollTop:$("#" + id).offset().top - 100}, 600);
+}
+
+function addListener() {
+    $(".listButton").on("click", function(e) {
+        let target = e.target.nextElementSibling.nextElementSibling;
+        if ($(target).hasClass("show")) {
+            return;
+        }
+        let h = $(target).height();
+        $(target).css("height","0px");
+        console.log(target);
+        $(target).addClass("show");
+        $(target).animate({"height": h}, 200, function() {
+            setTimeout(function(){    
+                $(target).css("height","");
+            },100);
+            resize();
+        });
+    });
+}
+
+isNaving = null;
+function NavTo(id) {
+    let targetX = $("#" + id).offset().left;
+    if (isNaving != null) {
+        clearInterval(isNaving);
+        isNaving = null
+    }
+    isNaving = setInterval(function() {
+        if (targetX == window.scrollX) {
+            clearInterval(isNaving);
+            isNaving = null;
+        }
+        let scrollOffset = 0;
+        if (targetX > window.scrollX) {
+            scrollOffset = 10;
+            if (targetX - window.scrollX < 10) scrollOffset = targetX - window.scrollX;
+        } else {
+            scrollOffset = -10;
+            if (targetX -window.scrollX>10) scrollOffset = targetX - window.scrollX;
+        }
+        window.scrollTo(window.scrollX + scrollOffset, window.scrollY);
+    }, 5);
+    resize();
+}
+
+var currentScene = 1;
+$("#toHome").on("click", function() {
+    NavTo("scene1");
+    $("#toHome").addClass("active");
+    $("#toSolution").removeClass("active");
+    $("#sidenav").removeClass("show")
+    currentScene = 1;
+})
+
+$("#toSolution").on("click", function() {
+    NavTo("scene2");
+    $("#toHome").removeClass("active");
+    $("#toSolution").addClass("active");
+    $("#sidenav").addClass("show")
+    currentScene = 2;
+})
+
+$("#toTop").on("click", function() {
+    navTo("body");
+})
+
--- js/loadSolutions.js
+++ js/loadSolutions.js
@@ -1,4 +1,4 @@
-var myData = null;
+var mySolutionsData = null;
 
 function generateTable(name,data) {
 	var html = `<div class="row transparent solutionList">`;
@@ -6,23 +6,73 @@ function generateTable(name,data) {
     html += `<div class="col-md-12 solutionListHead">${name}</div>`
 
 	for (var i = 0; i < data.count; i++) {
-        html += `<div class="col-md-10">${data.data[i].title}</div><div class="col-md-2">${data.data[i].date}</div>`;
+        html += `<div id="${name}-${i}" class="col-md-10 listButton">${data.data[i].title}</div><div class="col-md-2">${data.data[i].date}</div>`;
+        html += `<div class="col-md-12 listContent">${data.data[i].content}</div>`
 	}
 	html += `</div>`
 	return html;
 }
 
-function loadData() {
+function loadSolutionsData() {
 	$.get("solutions.json", function(e) {
-		myData = e;
-		if (typeof e == "string") myData = JSON.parse(myData);
+		mySolutionsData = e;
+		if (typeof e == "string") mySolutionsData = JSON.parse(mySolutionsData);
 		var html = "";
-		$.each(myData, function(key,value) {
+		$.each(mySolutionsData, function(key,value) {
 			html += generateTable(key,value);
 		})
 		$("#solutionList").html(html);
+		addListener();
 		resize();
 	});
 }
 
-loadData();
+loadSolutionsData();
+
+$("#searchInput").on("keyup", function(e) {
+	let text = $("#searchInput").val().toLocaleLowerCase().trim();
+	let items = searchForItems(text);
+	let html = generateSearchBox(items);
+	$("#searchBox").html(html);
+	$(".searchItem").on("click", function(e) {
+		let target = e.target;
+		if (!$(target).hasClass("searchItem")) {
+			target = target.parentElement;
+		}
+		console.log(target);
+		let targetid = $(target).attr("data-toggle");
+		$("#searchBox").html("");
+		navTo(targetid);
+		$(`#${targetid}`).click();
+	});
+});
+
+function searchForItems(text) {
+	if (mySolutionsData == null || text == "") return [];
+	var result =[];
+
+	$.each(mySolutionsData, function(key, data) {
+		for (var i = 0; i < data.count; i++) {
+			if (data.data[i].title.toLocaleLowerCase().indexOf(text) >= 0 ||
+				data.data[i].content.toLocaleLowerCase().indexOf(text) >= 0) {
+					result.push({
+						id: key + "-" + i,
+						title: data.data[i].title,
+						content: data.data[i].content.split("<br>")[0]
+					});
+			}
+		}
+	});
+	return result;
+}
+
+function generateSearchBox(items) {
+	var html = ""
+	for (var i=0; i < items.length; i++) {
+		html += `<div data-toggle="${items[i].id}" class="searchItem">`;
+		html += `<div class="searchHeader">【${items[i].id.split("-")[0]}】 ${items[i].title}</div>`
+		html += `<div class="searchContent">${items[i].content}</div>`
+		html += `</div>`
+	}
+	return html;
+}
